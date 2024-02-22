9d71bc6ed4979b7a0ca6fac73a564eec322da5a0
lukxnut <kristian2015ok@gmail.com>
2024-02-22 21:24:11 +0700

    Update template and style in ProductDisplay component

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="bg" :class="bgClass"></div>
+  <div v-if="dataProduct">
+    <div class="container">
+      <button @click="increment">Next Product</button>
+    </div>
+  </div>
+  <div v-else>
+    <div class="container unavailable-container">
+      <p>This product is unavailable to show</p>
+      <button @click="increment">Next Product</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -66,5 +77,51 @@ export default {
 
 .unavailable {
   background-color: var(--grey);
+}
+
+.container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+
+  width: 1034px;
+  height: 580px;
+
+  background-color: #fff;
+  border-radius: 10px;
+  margin: 125px auto;
+}
+
+.unavailable-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 13px;
+
+  background-image: url(../assets/sad-face.png);
+  background-repeat: no-repeat;
+  background-position: 65px center;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+}
+
+.unavailable-container p {
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+}
+
+.unavailable-container button {
+  font-weight: 600;
+  font-size: 20px;
+  color: var(--dark-brown);
+
+  background-color: transparent;
+  border: 3px solid #000;
+  border-radius: 4px;
+  padding: 9px 169px;
+
+  cursor: pointer;
 }
 </style>
